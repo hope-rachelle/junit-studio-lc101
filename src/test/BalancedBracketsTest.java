784136800b0c24fba2c51bbfc1aa9058da5442a9
Hope Ciuffa <hopeciuffa@gmail.com>
2021-09-16 18:26:50 -0500

    tests added

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,7 +1,7 @@
 package test;
 
 import org.junit.Test;
-
+import main.BalancedBrackets;
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
@@ -12,5 +12,33 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketsReturnTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+    @Test
+    public void onlyRightBracketFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+    @Test
+    public void onlyLeftBracketFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+    @Test
+    public void backwardBracketsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+    @Test
+    public void separationBracketsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Br[ack]ets"));
+    }
+    @Test
+    public void bracketsBeforeStringTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]Brackets"));
+    }
+    @Test
+    public void bracketsAfterStringTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Brackets[]"));
+    }
 
 }
